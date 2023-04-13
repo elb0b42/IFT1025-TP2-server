0b2173a9b31fd888f93f22c005afc59da400577b
Charles Bernard <charles.bernard9@hotmail.com>
2023-04-13 10:17:14 -0400

    Tache 3

--- src/main/java/Client/clientFX/Controler.java
+++ src/main/java/Client/clientFX/Controler.java
@@ -1,5 +1,23 @@
 package Client.clientFX;
 
-public class Controler {
+import server.models.Course;
+import server.models.RegistrationForm;
 
+import java.util.ArrayList;
+
+public class Controler {
+    private Model model;
+    private View view;
+
+    public Controler(Model model, View view) {
+        this.view = view;
+        this.model = model;
+    }
+    public void chargerListeCours(String session) {
+        ArrayList<Course> listeCours = model.demanderListeCours(session);
+
+    }
+    public void inscription(RegistrationForm form){
+        
+    }
 }
